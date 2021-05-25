de81f8c39203ac4fed0b258cbd529e6828084ead
Paul Winkler <paul.winkler@shopify.com>
2021-05-25 17:04:55 -0400

    install some packages in spin

--- install.sh
+++ install.sh
@@ -9,3 +9,9 @@ ln -sf $DOTFILES/bashrc .bashrc
 ln -sf $DOTFILES/gitconfig .gitconfig
 ln -sf $DOTFILES/dotemacs/.emacs .emacs
 ln -sf $DOTFILES/dotemacs/.emacs.d .emacs.d
+
+if [ $SPIN ]; then
+    for pkg in colordiff silversearcher-ag python-pygments; do
+        sudo apt-get install -y $pkg
+    done
+fi
