ae627236bd007ac965eba90bbcd06092c267feae
Paul Winkler <paul.winkler@shopify.com>
2021-05-25 17:11:12 -0400

    Cleaner emacs linking and byte compile all emacs stuff

--- install.sh
+++ install.sh
@@ -7,11 +7,17 @@ cd $HOME
 ln -sf $DOTFILES/bash_profile .bash_profile
 ln -sf $DOTFILES/bashrc .bashrc
 ln -sf $DOTFILES/gitconfig .gitconfig
-ln -sf $DOTFILES/dotemacs/.emacs .emacs
-ln -sf $DOTFILES/dotemacs/.emacs.d .emacs.d
+ln -sf $DOTFILES/dotemacs/.emacs .
+ln -sf $DOTFILES/dotemacs/.emacs.d .
 
 if [ $SPIN ]; then
     for pkg in colordiff silversearcher-ag python-pygments; do
         sudo apt-get install -y $pkg
     done
 fi
+
+echo
+echo "Byte compiling emacs"
+
+emacs --batch --eval '(byte-compile-directory "~/.emacs.d")'
+emacs --batch --eval '(byte-compile-file "~/.emacs")'
