a720bb611616b2f88c995e4f73ed8dcfccc0739f
Paul Winkler <paul.winkler@shopify.com>
2021-05-26 11:42:52 -0400

    Logging

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ ln -sf $DOTFILES/dotemacs/.emacs.d .
 
 if [ $SPIN ]; then
 
-    # Default shell
+    echo "Setting default shell to bash..."
     sudo chsh -s /bin/bash spin
 
-    # Extra packages
+    echo "Installing extra packages..."
     for pkg in colordiff silversearcher-ag python-pygments; do
         sudo apt-get install -y $pkg
     done
@@ -23,37 +23,36 @@ if [ $SPIN ]; then
     mkdir -p sh
     mkdir -p tmp
 
-    # diff-so-fancy needed by my git config
+    echo "Installing diff-so-fancy for my git config..."
     cd tmp
     git clone https://github.com/so-fancy/diff-so-fancy.git
     sudo mv -f diff-so-fancy/diff-so-fancy /usr/local/bin/
     sudo cp -r diff-so-fancy/lib /usr/local/bin
 
-    # Shell stuff
+    echo "Installing shell scripts..."
     git clone https://github.com/slinkp/sh.git
     cp -f sh/* ../sh/
 
     git clone https://github.com/slinkp/pw-git-scripts.git
     cp -f pw-git-scripts/* ../sh/
 
-    # Cleanup
+    echo "Cleanup..."
     cd $HOME
     rm -rf tmp
 
-    # Tags for emacs
+    echo "Tags support for emacs..."
     gem install ripper-tags
     ripper-tags -f TAGS -R -e components/ gems/ lib/ eagerlib/
+
+    echo
+    echo "Bootstrapping emacs packages"
+    yes | emacs --script $DOTFILES/emacs_bootstrap.el
+    echo
 fi
 
-echo
-echo "Byte compiling emacs"
+echo "Byte compiling elisp files"
 
-# First need to run to ensure use-package is installed
-yes | emacs --script $DOTFILES/emacs_bootstrap.el
-
-# Byte compile files
 emacs --batch --eval '(byte-compile-file (expand-file-name "~/.emacs"))'
-# Compile everything hopefully?
 emacs --batch --eval '(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)'
-# Redundant? Not sure
+# Not working, refresh harder! argh
 emacs --batch --eval '(byte-compile-file (expand-file-name "~/.emacs"))'
