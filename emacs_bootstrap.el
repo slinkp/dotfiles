d278fd4306fbcb972ff7bb88aea00d7ec95ee80d
Paul Winkler <paul.winkler@shopify.com>
2021-09-21 14:03:32 -0400

    UPdate emacs packages

--- emacs_bootstrap.el
+++ emacs_bootstrap.el
@@ -7,6 +7,6 @@
 (custom-set-variables
  '(package-selected-packages
    ;; Unfortunate to have to duplicate this from .emacs
-   '(yaml-mode aggressive-indent shadowenv helm-flycheck multi-web-mode git-link fill-column-indicator diminish sphinx-doc highlight-indentation flycheck s use-package pyvenv python-mode php-mode multiple-cursors markdown-preview-mode magit js2-mode jedi-core helm-projectile go-mode find-file-in-repository exec-path-from-shell dumb-jump ctable auto-complete)))
+   '(rg projectile-ripgrep helm-rg yaml-mode aggressive-indent shadowenv helm-flycheck multi-web-mode git-link fill-column-indicator diminish sphinx-doc highlight-indentation flycheck s use-package pyvenv python-mode php-mode multiple-cursors markdown-preview-mode magit js2-mode jedi-core helm-projectile go-mode find-file-in-repository exec-path-from-shell dumb-jump ctable auto-complete)))
 
 (package-install-selected-packages)
