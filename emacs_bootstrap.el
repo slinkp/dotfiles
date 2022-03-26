26cd692d05556004af0e1e20816c608d2661a96a
Paul Winkler <paul.winkler@shopify.com>
2022-03-26 00:34:26 -0400

    Update packages

--- emacs_bootstrap.el
+++ emacs_bootstrap.el
@@ -84,4 +84,4 @@
 (straight-use-package 'undo-tree)
 (straight-use-package 'lua-mode)
 (straight-use-package 'graphql-mode)
-(straight-use-package 'grip-mode) ;; another markdown preview, via github api, requires 'grip' command
+(straight-use-package 'protobuf-mode)
